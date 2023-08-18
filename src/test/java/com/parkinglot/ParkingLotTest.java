105f2fbc8018d082e01c3d09072fa09519264fdf
TACULMA2 <marigold.taculog@oocl.com>
2023-08-18 14:28:07 +0800

    test: add test case given parking lot and ticket and it fetched it will return the car.

--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -2,6 +2,7 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingLotTest {
@@ -14,6 +15,18 @@ public class ParkingLotTest {
      ParkingTicket parkingTicket = parkinglot.park(car);
      //then
         assertNotNull(parkingTicket);
+    }
+
+    @Test
+    void should_return_car_when_fetch_given_parking_lot_and_ticket() {
+    //given
+     ParkingLot parkingLot = new ParkingLot();
+     Car car = new Car();
+     ParkingTicket parkingTicket = parkingLot.park(car);
+     //when
+        Car fetchedCar = parkingLot.fetch(parkingTicket);
+     //then
+        assertEquals(car, fetchedCar);
     }
 
 
