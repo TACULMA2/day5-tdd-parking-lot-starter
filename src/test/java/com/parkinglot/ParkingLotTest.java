0a59aadceac42ee53f8f07d4c34ac7a7d42249b9
TACULMA2 <marigold.taculog@oocl.com>
2023-08-18 13:57:08 +0800

    feat: add classes; test: add test case: given parking lot and car then return parking ticket. Test passed.

---
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -0,0 +1,21 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_parking_ticket_when_park_the_car_given_parking_lot_and_car() {
+    //given
+     ParkingLot parkinglot = new ParkingLot();
+     Car car = new Car();
+     //when
+     ParkingTicket parkingTicket = parkinglot.park(car);
+     //then
+        assertNotNull(parkingTicket);
+    }
+
+
+}
+
