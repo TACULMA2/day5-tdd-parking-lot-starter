2284020863d662abe61de4e90b4e2a13001c9509
TACULMA2 <marigold.taculog@oocl.com>
2023-08-20 03:52:33 +0800

    test: add test case should_car_will_be_park_at_second_parking_lot_when_park_given_a_standard_boy_and_two_parking_lots_first_is_full_second_available()

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -23,4 +23,21 @@ public class SmartParkingBoyTest {
         assertEquals(9, firstParkingLot.getAvailableCapacity());
         assertEquals(19, secondParkingLot.getAvailableCapacity());
     }
+
+    @Test
+    void should_car_will_be_park_at_second_parking_lot_when_park_given_a_standard_boy_and_two_parking_lots_first_is_full_second_available() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot(0);
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car = new Car();
+        //when
+        ParkingTicket parkingTicket = smartParkingBoy.park(car);
+        //then
+        assertNotNull(parkingTicket);
+        assertEquals(0, firstParkingLot.getAvailableCapacity());
+        assertEquals(9, secondParkingLot.getAvailableCapacity());
+    }
+
 }
