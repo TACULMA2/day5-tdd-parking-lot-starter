f48f323b40c79303a60a52518ad2c91ee8be0804
TACULMA2 <marigold.taculog@oocl.com>
2023-08-20 03:55:49 +0800

    feat: add fetch method in SmartParkingBoy class.; Test: Add new test case should_return_right_car_when_fetch_car_twice_given_smart_parking_boy_two_parking_ticket() and modify the other tests name.

--- src/main/java/com/parkinglot/SmartParkingBoy.java
+++ src/main/java/com/parkinglot/SmartParkingBoy.java
@@ -16,4 +16,15 @@ public class SmartParkingBoy {
                 .orElseThrow(UnavailableParkingLotException::new);
         return parkingLotWithMostCapacity.park(car);
     }
+
+    public Car fetch(ParkingTicket parkingTicket) {
+        for (ParkingLot parkingLot : parkingLots) {
+            try {
+                return parkingLot.fetch(parkingTicket);
+            } catch (UnrecognizedTicketException ignored) {
+                // Catch and ignore the exception if the ticket is not recognized in this parking lot
+            }
+        }
+        throw new UnrecognizedTicketException(); // If the ticket is not recognized in any of the parking lots
+    }
 }
--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -40,4 +40,27 @@ public class SmartParkingBoyTest {
         assertEquals(9, secondParkingLot.getAvailableCapacity());
     }
 
+    @Test
+    void should_return_right_car_when_fetch_car_twice_given_standard_parking_boy_two_parking_ticket() {
+        //given
+        ParkingLot firstParkingLot = new ParkingLot();
+        ParkingLot secondParkingLot = new ParkingLot();
+        List<ParkingLot> parkingLots = List.of(firstParkingLot, secondParkingLot);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(parkingLots);
+        Car car1 = new Car();
+        Car car2 = new Car();
+        //when
+        ParkingTicket parkingTicket1 = smartParkingBoy.park(car1);
+        ParkingTicket parkingTicket2 = smartParkingBoy.park(car2);
+        Car fetchedCar1 = smartParkingBoy.fetch(parkingTicket1);
+        Car fetchedCar2 = smartParkingBoy.fetch(parkingTicket2);
+        //then
+        assertNotNull(parkingTicket1);
+        assertNotNull(parkingTicket2);
+        assertEquals(car1, fetchedCar1);
+        assertEquals(car2, fetchedCar2);
+        assertEquals(10, firstParkingLot.getAvailableCapacity());
+        assertEquals(10, secondParkingLot.getAvailableCapacity());
+    }
+
 }
